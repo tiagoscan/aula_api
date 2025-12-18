8b0b29e4101020ef1c603b2680b02b0df9c77f4f
Tiago Candido <tiagocan@gmail.com>
2025-12-17 23:57:29 -0300

    feat: implementado método POST

--- app/src/main/java/com/example/aulaapi/api/PostagemAPI.kt
+++ app/src/main/java/com/example/aulaapi/api/PostagemAPI.kt
@@ -3,8 +3,11 @@ package com.example.aulaapi.api
 import com.example.aulaapi.model.Comentario
 import com.example.aulaapi.model.Postagem
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface PostagemAPI {
 
@@ -19,8 +22,21 @@ interface PostagemAPI {
 
     @GET("posts/{id}/comments")
     suspend fun recuperarComentariosParaPostagem(
-        @Path("id") id: Int
+        @Query("id") id: Int
     ): Response<List<Comentario>>
+
+    @GET("comments")
+    suspend fun recuperarComentariosParaPostagemQuery(
+        @Query("postId") id: Int
+    ): Response<List<Comentario>>
+
+    @POST("posts")
+    suspend fun salvarPostagem(
+        @Body postagem: Postagem
+    ): Response<Postagem>
+
+
+
 }
 
 
